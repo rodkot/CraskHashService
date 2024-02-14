3815bc20eab4fd42f9022a676831f64af23720e3
Rodion Kotov <r.kotov@rodkot.site>
2024-02-14 19:05:04 +0700

    Инициализация проекта

---
+++ Manager/src/test/kotlin/ru/nsu/ccfit/manager/ManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ Worker/src/main/kotlin/ru/nsu/ccfit/worker/WorkerApplication.kt
@@ -0,0 +1,11 @@
+package ru.nsu.ccfit.worker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkerApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorkerApplication>(*args)
+}
